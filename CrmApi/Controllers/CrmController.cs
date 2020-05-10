dfe73b2418585734c53e06a8723823c51c459fb3
elenanto <elenanto101@gmail.com>
2020-05-10 14:04:46 +0300

    add API project

---
+++ CrmApi/Controllers/CrmController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CrmApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CrmController : ControllerBase
+    {
+        private readonly ILogger<CrmController> _logger;
+
+        public CrmController(ILogger<CrmController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "Welcome to our Site!" + "\n" + DateTime.Now;
+        }
+    }
+}
